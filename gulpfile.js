c5808829d6c5b055dd187e7b9b57e85b582fda2b
pallavi1912 <vermapallavi93@gmail.com>
2016-09-06 23:53:22 +0530

    required gulp files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+/*require plugin*/
+var gulp=require('gulp'),
+    gulpbrowserify=require('gulp-browserify'),
+    concat=require('gulp-concat'),
+    gulpIf=require('gulp-if'),
+    minify=require('gulp-minify-css'),
+    ngAnnotate=require('gulp-ng-annotate'),
+    rename=require('gulp-rename'),
+    sass=require('gulp-sass'),
+    shell=require('gulp-shell'),
+    uglify=require('gulp-uglify'),
+    stringify=require('stringify');
+
+
+   
