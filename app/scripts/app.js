550414ece0dfa4bb32c2a0e46d70eeea458040b6
pallavi1912 <vermapallavi93@gmail.com>
2016-09-11 14:12:51 +0530

    app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+var API = require('./config/api'),
+    blogsApp=angular.module('blogsApp',[
+    	require('./common').name,
+    	require('./modules').name,
+    	require('./shared').name])
+    .run(['$rootScope', '$state', function($rootScope, $state){
+    	console.log("in run");	
+    }])
+    .config(['urlRouterProvider', 'toastrConfig', 'AppConfig',function($urlRouterProvider, toastrConfig, AppConfig){
+    	angular.extend(toastrConfig, AppConfig.toastr);
+    	console.log("in config");
+    }]);
+    function bootstrapApplication() {
+     angular.element(document).ready(function() {
+         angular.bootstrap(document, ["blogsApp"]);
+     });
+ }
+ bootstrapApplication();
