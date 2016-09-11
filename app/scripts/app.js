0dd495f1586c34b003043c5bfc26935a42c5e702
pallavi1912 <vermapallavi93@gmail.com>
2016-09-11 16:12:38 +0530

    routing done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
 'use strict';
 var API = require('./config/api'),
     blogsApp=angular.module('blogsApp',[
+    	'ui.router',
+         'ui.bootstrap',
+         // 'sun.scrollable',
+         'toastr',
+         // 'angular-ui-validator',
+         // 'ngMessages',
+         // 'ngFileUpload',
+         // 'gm.datepickerMultiSelect',
+         // 'ngSanitize',
+         // 'ui.select',
     	require('./common').name])
     	//require('./common').name
     	/*require('./modules').name,
