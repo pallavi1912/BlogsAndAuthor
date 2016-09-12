8acbee19d84618e99b63a4ce70d01d63ac7e3869
pallavi1912 <vermapallavi93@gmail.com>
2016-09-12 23:35:54 +0530

    index.js file modified

--- app/scripts/modules/blogs/index.js
+++ app/scripts/modules/blogs/index.js
@@ -5,9 +5,8 @@ module.exports=angular.module('Blogs',[])
     	   .state('app.blogList',{
     	   	url:"^/blogList",
             moduleName:'blogs',
-    	   	template: require('./templates/blogList.html')
-    	   }),
-    	  resolve: {
+    	   	template: require('./templates/blogList.html'),
+    	   	resolve: {
           blogList: ['$q', 'blogListService', function($q, blogListService) {
             var deffered = $q.defer();
             blogListService.getBlogList()
@@ -21,6 +20,7 @@ module.exports=angular.module('Blogs',[])
             return deffered.promise;
           }]
         }
+    	   })
     }])
     .service('blogListService',require('./services/blog-list-service.js'))
     .directive('blogListDirective',require('./directives/blogListDirective.js'));
