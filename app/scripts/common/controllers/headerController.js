133d0a09de6952e7347366837e345cb94a0636a8
Pallavi Verma <vermapallavi93@gmail.com>
2016-09-26 14:41:54 +0530

    url for modal

--- app/scripts/common/controllers/headerController.js
+++ app/scripts/common/controllers/headerController.js
@@ -29,7 +29,7 @@ module.exports = [
     var modalInstance= $uibModal.open({
       animation:false,
       backdrop:'static',
-      template:'<h1>hello</h1>'
+      templateUrl:'../templates/login.html'
     });
    };
     
